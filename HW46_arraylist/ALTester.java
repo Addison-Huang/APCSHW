881b616257f5ad0ecb825a04fb58ab943b8ba36b
ahuang20stuy <ahuang20@stuy.edu>
2017-12-04 19:45:31 -0500

    HW46_arraylist/

---
+++ HW46_arraylist/ALTester.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class ALTester {
+
+    /* public static void Populate(int length) {
+	int counter = 0;
+	while (counter < length-1) {
+	    ArrayList.add(counter);
+	    counter += 1;
+	}
+	}*/
+	
+	
+
+    public static void main(String[] args) {
+	ArrayList joe = new ArrayList();
+        int counter = 0;
+	while (counter < 22) {
+	    ArrayList.add(counter);
+	    counter += 1;
+        }
+    }
+}
