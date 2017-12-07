afd909af42238b3392a09f28845acb32b8bf9b2e
Addison <Addison@android-fce9a541a0403111.home>
2017-12-06 19:08:21 -0500

    HW48

--- HW48_GuessANumber/GuessNumber.java
+++ HW48_GuessANumber/GuessNumber.java
@@ -1,17 +1,113 @@
-import cs1.keyboard;
-public class GuessNumber {
+/*==================================================
+  class GuessNumber -- fun fun fun!
+  eg, sample interaction with end user:
+  Guess a # fr 1-100: 50
+  Too high
+  Guess a # fr 1-49: 25
+  Too low
+  Guess a # fr 26-49: 38
+  Correct! It took 3 guesses
+  ==================================================*/
+
+import cs1.Keyboard;
+
+public class GuessNumber 
+{
     //instance vars
-    private int upperBound = 100; //stores the upperBound of guesses
-    private int lowerBound = 1; //stores the lowerBound of gueeses
-    private int numberGuess = (int)(Math.random()*100) +1 ; //stores the numberToGuess
-    private int guesses = 0; //stores the # of guesses
+    private int _lo, _hi, _guessCtr, _target;
 
-    public static void guess(int guess) {
 
-	if (guess > upperBound) {
-	    System.out.println("Oops! Too high. Try again!");
-	    
-	
+    /*==================================================
+      constructor -- initializes a guess-a-number game
+      pre:  
+      post: _lo is lower bound, _hi is upper bound,
+      _guessCtr is 1, _target is random int on range [_lo,_hi]
+      ==================================================*/
+    public GuessNumber( int a, int b ) 
+    {
+	_lo = a;
+	_hi = b;
+	_guessCtr = 0;
+	_target = (int)(Math.random()*(_hi - _lo)) + _lo;
     }
 
-    
+
+    /*==================================================
+      void playRec() -- Prompts a user to guess until guess is correct.
+      Uses recursion.
+      pre:  
+      post: 
+      ==================================================*/
+    public void playRec() 
+    {
+	System.out.print("Guess a number from " + _lo + " to " + _hi + ": ");
+	int guess1 = Keyboard.readInt();
+	_guessCtr += 1;
+	if (guess1 == _target) {
+	    System.out.println("WOOO YOU GUESSED THE CORRECT NUMBER");
+	    System.out.println("It took " + _guessCtr + " tries");
+	}
+	else if (guess1 > _target) {
+	    System.out.println("Your guess was too high");
+	    _hi = guess1 - 1;
+	    playRec();
+	} else {
+	    System.out.println("Your guess was too low");
+	    _lo = guess1 + 1;
+	    playRec();
+	}	
+    }
+
+
+    /*==================================================
+      void playIter() -- Prompts a user to guess until guess is correct.
+      Uses iteration.
+      pre:  
+      post: 
+      ==================================================*/
+    public void playIter() 
+    {
+	System.out.print("Guess a number from " + _lo + " to " + _hi + ": ");
+	int guess1 = Keyboard.readInt();
+	while (guess1 != _target) {
+	    _guessCtr += 1;
+	    if (guess1 > _target) {
+		System.out.println("Your guess was too high");
+		_hi = guess1 - 1;
+	    } else {
+		System.out.println("Your guess was too low");
+		_lo = guess1 + 1;
+	    }
+	    System.out.print("Guess a number from " + _lo + " to " + _hi + ": ");
+	    guess1 = Keyboard.readInt();
+	}
+	_guessCtr += 1;
+	System.out.println("");
+	System.out.println("WOOO YOU GUESSED THE CORRECT NUMBER!!!!!!");
+	System.out.println("!!!~~~~~~~~~ CONGRATULATIONS ~~~~~~~~~!!!");
+	System.out.println("It took " + _guessCtr + " tries");	
+    }
+
+
+    //wrapper for playRec/playIter to simplify calling
+    public void play() 
+    {
+	System.out.println("~~~Welcome to Guess Number!~~~");
+	System.out.println("");
+	//use one or the other below:
+	//playRec();
+	playIter();
+    }
+
+
+    //main method to run it all
+    public static void main( String[] args ) 
+    {
+	//instantiate a new game
+	GuessNumber g = new GuessNumber(1,100);
+	//start the game
+	g.play();
+    }//end main
+
+}//end classimport cs1.keyboard;
+
