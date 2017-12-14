9683e7ee322add9c899b0142d396c5c16bd28c3a
ahuang20stuy <ahuang20@stuy.edu>
2017-12-13 19:46:50 -0500

    hw53

---
+++ hw53_solidComparativeAnalysis/MySorts/SortTester.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+
+
+public class SortTester {
+    //~~~~~~~~~~~~~~~~~~~ HELPER METHODS ~~~~~~~~~~~~~~~~~~~
+  //precond: lo < hi && size > 0
+  //postcond: returns an ArrayList of random integers
+  //          from lo to hi, inclusive
+  public static ArrayList populate( int size, int lo, int hi ) {
+    ArrayList<Integer> retAL = new ArrayList<Integer>();
+    while( size > 0 ) {
+	    //     offset + rand int on interval [lo,hi]
+	    retAL.add( lo + (int)( (hi-lo+1) * Math.random() ) );
+	    size--;
+    }
+    return retAL;
+  }
+
+  //randomly rearrange elements of an ArrayList
+  public static void shuffle( ArrayList al ) {
+    int randomIndex;
+    for( int i = al.size()-1; i > 0; i-- ) {
+	    //pick an index at random
+      randomIndex = (int)( (i+1) * Math.random() );
+	    //swap the values at position i and randomIndex
+      al.set( i, al.set( randomIndex, al.get(i) ) );
+    }
+  }
+  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    public static void main( String [] args )
+  {
+    System.out.println("\n*** Testing sort-in-place (void) version... *** ");
+    ArrayList glen = new ArrayList<Integer>();
+    glen.add(7);
+    glen.add(1);
+    glen.add(5);
+    glen.add(12);
+    glen.add(3);
+    System.out.println( "\nArrayList glen before sorting:\n" + glen );
+    MySorts.insertionSortV(glen);
+    System.out.println( "\nArrayList glen after sorting:\n" + glen );
+
+    ArrayList coco = populate( 10, 1, 1000 );
+    System.out.println( "\nArrayList coco before sorting:\n" + coco );
+    MySorts.insertionSortV(coco);
+    System.out.println( "\nArrayList coco after sorting:\n" + coco );
+    /*===============for VOID methods=============
+      ============================================*/
+
+    /*==========for AL-returning methods==========
+      System.out.println( "*** Testing non-void version... *** " );
+    	ArrayList glen = new ArrayList<Integer>();
+      glen.add(7);
+      glen.add(1);
+      glen.add(5);
+      glen.add(12);
+      glen.add(3);
+      System.out.println( "\nArrayList glen before sorting:\n" + glen );
+      ArrayList glenSorted = insertionSort( glen );
+      System.out.println( "\nsorted version of ArrayList glen:\n" 
+      + glenSorted );
+      System.out.println( "\nArrayList glen after sorting:\n" + glen );
+
+      ArrayList coco = populate( 10, 1, 1000 );
+      System.out.println( "\nArrayList coco before sorting:\n" + coco );
+      ArrayList cocoSorted = insertionSort( coco );
+      System.out.println( "\nsorted version of ArrayList coco:\n" 
+      + cocoSorted );
+      System.out.println( "\nArrayList coco after sorting:\n" + coco );
+      System.out.println( coco );
+      ============================================*/
+
+  }//end main
+}
