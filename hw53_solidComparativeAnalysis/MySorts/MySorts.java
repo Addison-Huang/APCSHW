f5621d8102c41eed00012d9353b462eb6f714f76
Addison <Addison@Ryans-MBP.home>
2017-12-18 23:27:11 -0500

    hw56

--- hw53_solidComparativeAnalysis/MySorts/MySorts.java
+++ hw53_solidComparativeAnalysis/MySorts/MySorts.java
@@ -1,50 +1,65 @@
+/*Team Unicorn Dogs -- Muhammad Kashf Mashrafi and Addison Huang
+  APCS pd1
+  HW #55: Never Fear, Big Oh Is Here!
+  2017 12 13 */
+
+
 import java.util.ArrayList ; 
 public class MySorts {
-    // VOID version of bubbleSort
-    // Rearranges elements of input ArrayList
-    // postcondition: data's elements sorted in ascending order
-    public static void bubbleSortV( ArrayList<Comparable> data )
-    {
+
+    /* In a best case scenario, bubbleSort is O(n). This is because bubbleSort will simply just iterate through the ArrayList without making any changes. It'll take n passes to iterate an ArrayList of size n. In a worst case scenario, bubbleSort is O(N^2). It sorts only once per iteration and compares N-i elements every pass, where N is the size of ArrayList and i is the pass number.
+     */
+    public static void bubbleSort( ArrayList<Comparable> data) {
+	int swapCtr = 0;
+	int comparisonCtr = 0;
 	//make n-1 passes across collection
 	for( int passCtr = 1; passCtr < data.size(); passCtr++ ) {
 	    System.out.println( "commencing pass #" + passCtr + "..." );
-
 	    //iterate from first to next-to-last element
 	    for( int i = 0; i < data.size()-1; i++ ) {
 		//if element at i > element at i+1, swap
-		if ( data.get(i).compareTo(data.get(i+1) ) > 0 ) 
-		    data.set( i, data.set(i+1,data.get(i)) );	
-		//System.out.println(data); //diag: show current state of list
+		comparisonCtr += 1;
+		if ( data.get(i).compareTo(data.get(i+1) ) > 0 ) {
+		    swapCtr += 1;
+		    data.set( i, data.set(i+1,data.get(i)));
+		    System.out.println("Index " + i + " swapped with index" + (i + 1));		       
+		}
 	    }
 	}
+	System.out.println("There was a total of " + swapCtr + " swaps made and " + comparisonCtr + " comparisons made.");
     }
+				       
 
-    // VOID version of SelectionSort
-    // Rearranges elements of input ArrayList
-    // postcondition: data's elements sorted in ascending order
-    public static void selectionSortV( ArrayList<Comparable> data ) 
+    /*The best and worst case of selectionSort is O(n^2). With every pass, it will compare n - pass# elements, where n is the size of the ArrayList. */
+    public static void selectionSort( ArrayList<Comparable> data ) 
     {
 	//note: this version places greatest value at rightmost end,
 
 	//maxPos will point to position of SELECTION (greatest value)
 	int maxPos;
-
+	int swapCtr = 0;
+	int comparisonCtr = 0;
 	for( int pass = data.size()-1; pass > 0; pass-- ) {
-	    System.out.println( "\nbegin pass " + (data.size()-pass) );//diag
+	    System.out.println( "\nbegin pass # " + (data.size()-pass) );//diag
 	    maxPos = 0;
 	    // System.out.println(data);
 	    for( int i = 1; i <= pass; i++ ) {
-		System.out.println( "maxPos: " + maxPos );//diag
-		System.out.println( data );//diag
+		//System.out.println( "maxPos: " + maxPos );//diag
+		//System.out.println( data );//diag
+		comparisonCtr += 1;
 		if ( data.get(i).compareTo( data.get(maxPos) ) > 0 )
 		    maxPos = i;
 	    }
 	    data.set( maxPos, ( data.set( pass, data.get(maxPos) ) ) );
+	    swapCtr += 1;
 	    System.out.println( "after swap: " +  data );//diag
 	}
-    } //end selectionSortV
+	System.out.println("There was a total of " + swapCtr + " swaps made and " + comparisonCtr + " comparisons made.");
+	
+    } //end selectionSort
 
-    public static void insertionSortV( ArrayList<Comparable> data )
+    /* The best case of InsertionSort is O(N). It will only pass through the array once in the best case scenario. In the worst case scenario it will be O(N^2). In the worst case scenario it will compare (N-1) + (N-2) + (N-3) ... 2 + 1 elements. Using Gauss' formula, we will be comparing n(n-1+1)/2 elements, which is bound by n^2 */ 
+    public static void insertionSort( ArrayList<Comparable> data )
     {
 	for( int partition = 1; partition < data.size(); partition++ ) {
 	    //partition marks first item in unsorted region
@@ -67,7 +82,7 @@ public class MySorts {
 		    break; 
 	    }
 	}
-    }//end insertionSortV
+    }//end insertionSort
 
        
 }
--- hw53_solidComparativeAnalysis/MySorts/SortTester.java
+++ hw53_solidComparativeAnalysis/MySorts/SortTester.java
@@ -1,75 +1,95 @@
+/*Team Unicorn Dogs -- Muhammad Kashf Mashrafi and Addison Huang
+  APCS pd1
+  HW #55: Never Fear, Big Oh is here!
+  2017 12 14 */
+
 import java.util.ArrayList;
 
-
 public class SortTester {
-    //~~~~~~~~~~~~~~~~~~~ HELPER METHODS ~~~~~~~~~~~~~~~~~~~
-  //precond: lo < hi && size > 0
-  //postcond: returns an ArrayList of random integers
-  //          from lo to hi, inclusive
-  public static ArrayList populate( int size, int lo, int hi ) {
-    ArrayList<Integer> retAL = new ArrayList<Integer>();
-    while( size > 0 ) {
-	    //     offset + rand int on interval [lo,hi]
-	    retAL.add( lo + (int)( (hi-lo+1) * Math.random() ) );
-	    size--;
-    }
-    return retAL;
-  }
+    public static void main( String [] args ) {
+	System.out.println("--- Best and Worst Case Scenario of BubbleSort ");
+	/*In BubbleSort, the best case scenario is when ArrayList is ordered in a way that any value at index i will always be less than the value at index i + 1. This is because in bubbleSort, a swap will only be made if the index at i is greater than the index at i + 1. There will always be the same amount of comparisons done in total for each ArrayList of size n, but there will be a different amount of swaps everytime. The best case scenario is the one that takes up the least memory, and swaps take more memory then comparisons. Therefore, the ArrayList that takes the least amount of swaps is the best case scenario. The inverse of this statement is also true. The worst case scenario is the one that takes the most amount of swaps, which is when the ArrayList is ordered in a way that any value at index i is greater than the value at index i +1.
+	 
+	System.out.println("---- Start Best Case  ---- ");
+	ArrayList<Comparable> bestCaseBub = new ArrayList<Comparable>();
+	bestCaseBub.add(1);
+	bestCaseBub.add(3);
+	bestCaseBub.add(5);
+	bestCaseBub.add(7);
+        bestCaseBub.add(12);
+	System.out.println(bestCaseBub);
+	MySorts.bubbleSort(bestCaseBub);
+	System.out.println(bestCaseBub);
+	System.out.println("---- End Best Case ----" );
+	System.out.println("");
+	System.out.println("---- Start Worst Case ----");
+	ArrayList<Comparable> worstCaseBub = new ArrayList<Comparable>();
+	worstCaseBub.add(12);
+	worstCaseBub.add(7);
+	worstCaseBub.add(5);
+	worstCaseBub.add(3);
+        worstCaseBub.add(1);
+	System.out.println(worstCaseBub);
+	MySorts.bubbleSort(worstCaseBub);
+	System.out.println(worstCaseBub);
+	System.out.println("---- End Worst Case  ----");
+	System.out.println("");
 
-  //randomly rearrange elements of an ArrayList
-  public static void shuffle( ArrayList al ) {
-    int randomIndex;
-    for( int i = al.size()-1; i > 0; i-- ) {
-	    //pick an index at random
-      randomIndex = (int)( (i+1) * Math.random() );
-	    //swap the values at position i and randomIndex
-      al.set( i, al.set( randomIndex, al.get(i) ) );
-    }
-  }
-  //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	System.out.println("--- Best and Worst Case Scenario of SelectionSort ");
+	/*There is no best or worst case scenario of SelectionSort. In slection sort, a swap is made with every pass, therefore there will always be a total of n-1 passes in an ArrayList of size n. Even if a number is instaintated in it's FRP, it is swapped with itself. */
+        
+	System.out.println("---- Start Best Case  ---- ");
+	ArrayList<Comparable> bestCaseSel = new ArrayList<Comparable>();
+	bestCaseSel.add(7);
+	bestCaseSel.add(1);
+	bestCaseSel.add(5);
+	bestCaseSel.add(12);
+        bestCaseSel.add(3);
+	System.out.println(bestCaseSel);
+	MySorts.selectionSort(bestCaseSel);
+	System.out.println(bestCaseSel);
+	System.out.println("---- End Best Case ----" );
+	System.out.println("");
+	System.out.println("---- Start Worst Case  ----");
+	ArrayList<Comparable> worstCaseSel = new ArrayList<Comparable>();
+	worstCaseSel.add(12);
+	worstCaseSel.add(7);
+	worstCaseSel.add(5);
+	worstCaseSel.add(3);
+        worstCaseSel.add(1);
+	System.out.println(worstCaseSel);
+	MySorts.selectionSort(worstCaseSel);
+	System.out.println(worstCaseSel);
+	System.out.println("---- End Worst Case ----");
+	System.out.println("");
 
-    public static void main( String [] args )
-  {
-    System.out.println("\n*** Testing sort-in-place (void) version... *** ");
-    ArrayList glen = new ArrayList<Integer>();
-    glen.add(7);
-    glen.add(1);
-    glen.add(5);
-    glen.add(12);
-    glen.add(3);
-    System.out.println( "\nArrayList glen before sorting:\n" + glen );
-    MySorts.insertionSortV(glen);
-    System.out.println( "\nArrayList glen after sorting:\n" + glen );
+	System.out.println("--- Best and Worst Case Scenario of InsertionSort ");
+	/* The worst case scenario of InsertionSort is when the ArrayList is presented in a decending order, much like BubbleSort. This is because multiple swaps must be made to get the last index of the newly expanded partition all the way to the front of the ArrayList, to put it in acending order. The best case scenario is when the list is already in acending order because as the partition range increases, swaps are not made because the ArrayList is already sorted.*/
+	System.out.println("---- Start Best Case ---- ");
+	ArrayList<Comparable> bestCaseIns = new ArrayList<Comparable>();
+	bestCaseIns.add(12);
+	bestCaseIns.add(7);
+	bestCaseIns.add(5);
+	bestCaseIns.add(3);
+        bestCaseIns.add(1);
+	System.out.println(bestCaseIns);
+	MySorts.insertionSort(bestCaseIns);
+	System.out.println(bestCaseIns);
+	System.out.println("---- End Best Case ----" );
+	System.out.println("");
+	System.out.println("---- Start Worst Case ----");
+	ArrayList<Comparable> worstCaseIns = new ArrayList<Comparable>();
+	worstCaseIns.add(1);
+	worstCaseIns.add(3);
+	worstCaseIns.add(5);
+	worstCaseIns.add(7);
+        worstCaseIns.add(12);
+	System.out.println(worstCaseIns);
+	MySorts.insertionSort(worstCaseIns);
+	System.out.println(worstCaseIns);
+	System.out.println("---- End Worst Case ----");
+	System.out.println("");
+	
 
-    ArrayList coco = populate( 10, 1, 1000 );
-    System.out.println( "\nArrayList coco before sorting:\n" + coco );
-    MySorts.insertionSortV(coco);
-    System.out.println( "\nArrayList coco after sorting:\n" + coco );
-    /*===============for VOID methods=============
-      ============================================*/
-
-    /*==========for AL-returning methods==========
-      System.out.println( "*** Testing non-void version... *** " );
-    	ArrayList glen = new ArrayList<Integer>();
-      glen.add(7);
-      glen.add(1);
-      glen.add(5);
-      glen.add(12);
-      glen.add(3);
-      System.out.println( "\nArrayList glen before sorting:\n" + glen );
-      ArrayList glenSorted = insertionSort( glen );
-      System.out.println( "\nsorted version of ArrayList glen:\n" 
-      + glenSorted );
-      System.out.println( "\nArrayList glen after sorting:\n" + glen );
-
-      ArrayList coco = populate( 10, 1, 1000 );
-      System.out.println( "\nArrayList coco before sorting:\n" + coco );
-      ArrayList cocoSorted = insertionSort( coco );
-      System.out.println( "\nsorted version of ArrayList coco:\n" 
-      + cocoSorted );
-      System.out.println( "\nArrayList coco after sorting:\n" + coco );
-      System.out.println( coco );
-      ============================================*/
-
-  }//end main
+    }//end main
 }
