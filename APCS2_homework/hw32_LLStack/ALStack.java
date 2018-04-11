0b5e87cdf25e951a12544ce1edc64d03d13d53f4
ahuang20stuy <ahuang20@stuy.edu>
2018-04-11 09:23:45 -0400

    hw32

---
+++ APCS2_homework/hw32_LLStack/ALStack.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class ALStack<PANCAKE> implements Stack<PANCAKE>{
+
+    private ArrayList<PANCAKE> stack;
+
+    public ALStack() {
+	ArrayList stack = new ArrayList();
+    }
+    
+    public boolean isEmpty() {
+	return (stack.size() == 0);
+    }
+
+    public PANCAKE peek() {
+	return stack.get(0);
+    }
+
+    public PANCAKE pop() {
+	return stack.remove(0);
+    }
+
+    public void push(PANCAKE x) {
+	stack.add(0, x);
+    }
+
+    public static void main(String[] args) {
+    }
+}
